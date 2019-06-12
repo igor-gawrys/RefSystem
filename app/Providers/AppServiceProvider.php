11d6aff4d40478f088269024474f5f637ed99224
igor gawrys <prezes@socialler.pl>
2019-06-12 22:48:20 +0200

    Repair some errors!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Router;
+
 use App\Ref;
 
 use App\Observers\RefObserver;
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
     {
         
        Ref::observe( new RefObserver );
+
+       Route::bind( 'refs', function( $value ) {
+
+            return Ref::where( 'slug', $value )->firstOrFail();
+
+       });
 
     }
 
